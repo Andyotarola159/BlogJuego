2bd34228bb658f0b369fef928bdd0c56eb32cfcb
Andyotarola <andyotarola8@gmail.com>
2017-03-23 19:29:19 -0500

    menu para celulares

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-$(document).foundation()
+var menu = document.getElementById("ejecutar");
+var bars = document.getElementById("bars");
+var bars2 = document.getElementById("bars2")
+var bars4 = document.getElementById("fa-bars4")
+var bars5 = document.getElementById("fa-bars5")
+
+bars.addEventListener("click",cambiar);
+bars2.addEventListener("click",regresar)
+
+
+function cambiar() {
+	
+	menu.style="display:none";
+	bars4.style="display:block";
+	bars5.style="display:none";
+
+	
+}
+function regresar(){
+    menu.style="display:block";
+    bars5.style="display:block";
+    bars4.style="display:none";
+}
